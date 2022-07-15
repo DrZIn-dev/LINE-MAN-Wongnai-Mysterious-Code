f697d0aae046cd276b1c590e8ecccf15e8bd65de
DrZIn-dev <quick.car6020@fastmail.com>
2022-07-15 13:40:14 +0700

    feat: sumbit ans

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	var whatIsIt string
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+	for _, v := range string(sd) {
+		whatIsIt = string(v) + whatIsIt
+	}
+	fmt.Println("Result : ", whatIsIt)
+}
